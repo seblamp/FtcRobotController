14d29f4195a61fdcbc603338cf86f73b76068901
AdminCuivreOr <168549335+seblamp@users.noreply.github.com>
2026-07-06 23:50:55 -0400

    Update GamePadPractice.java
    Practice with GamePad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadPractice.java
@@ -15,9 +15,28 @@ public class GamePadPractice extends OpMode {
     public void loop() {
         // runs 50 times per second
         double speedForward = -gamepad1.left_stick_y /2.0;
+        double speedFowardY = -gamepad1.right_stick_y/2.0;
+        double diffXJoysticks = gamepad1.left_stick_x - gamepad1.right_stick_x;
+        double sumTriggers = gamepad1.left_trigger + gamepad1.right_trigger;
 
-        telemetry.addData("x",gamepad1.left_stick_x);
-        telemetry.addData("y", speedForward);
+        telemetry.addData("Left X",gamepad1.left_stick_x);
+        telemetry.addData("Left Y", speedForward);
+        telemetry.addData("Right X", gamepad1.right_stick_x);
+        telemetry.addData("Right Y", speedFowardY);
+
+        telemetry.addData("Difference left and right X",diffXJoysticks);
+        telemetry.addData("Sum Triggers", sumTriggers);
+
         telemetry.addData("A button",gamepad1.a);
+        telemetry.addData("B button", gamepad1.b);
+
     }
+    /*
+    1. Add telemetry for the right joystick
+    2. Add telemetry for the B button
+    3. Add telemetry to report the difference between x left joystick and x right joystick
+    4. Add telemetry to report the sum of the two triggers
+
+     */
+
 }

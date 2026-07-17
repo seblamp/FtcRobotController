f07017b015223e7fafbb86dba6b639a4d04205a4
AdminCuivreOr <168549335+seblamp@users.noreply.github.com>
2026-07-16 21:03:58 -0400

    Autonome qui utilise le IMU pour tourner

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClassBotAutoCMandIMU.java
@@ -0,0 +1,105 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.mechanisms.ClassBotArm;
+import org.firstinspires.ftc.teamcode.mechanisms.ClassBotClaw;
+import org.firstinspires.ftc.teamcode.mechanisms.ClassBotDrive;
+
+@Autonomous(name ="TestAutoClassBotWithIMUandCM")
+public class ClassBotAutoCMandIMU extends OpMode {
+    ClassBotDrive drivetrain = new ClassBotDrive();
+    ClassBotArm arm = new ClassBotArm();
+    ClassBotClaw claw = new ClassBotClaw();
+
+    enum State {
+        FORWARD,
+        TURN,
+        RAISE_ARM_CLOSE_CLAW,
+        FINISHED
+    }
+
+    State state = State.FORWARD;
+
+    boolean stateStarted = false;
+
+
+    @Override
+    public void init() {
+        drivetrain.init(hardwareMap);
+        arm.init(hardwareMap);
+        claw.init(hardwareMap);
+        arm.resetArmEncoder();
+        drivetrain.resetDriveEncoders();
+        claw.open();
+        state = State.FORWARD;
+        stateStarted = false;
+
+    }
+
+    @Override
+    public void loop() {
+
+        telemetry.addData("Current State", state);
+        switch (state) {
+            case FORWARD:
+                if (!stateStarted) {
+                    drivetrain.driveDistance(50, 0.2);  //avance de 50 cm à 0.2 de power
+                    stateStarted = true;
+                }
+                if (!drivetrain.isDriveBusy()) {          //if (!drivetrain.isDriveBusy() && !arm.isArmBusy()) { //Si on fait aussi bouger le bras
+                    state = State.TURN;
+                    stateStarted = false;
+                }
+                break;
+
+            case TURN:
+                if (!stateStarted) {
+                    drivetrain.turnDegrees("RIGHT", 90, 0.2);
+                    stateStarted = true;
+                }
+
+                if (!drivetrain.isTurnBusy()) {
+                    state = State.RAISE_ARM_CLOSE_CLAW;
+                    stateStarted = false;
+                }
+                break;
+
+            case RAISE_ARM_CLOSE_CLAW:
+                if (!stateStarted) {
+                    arm.setArmUp(0.6);
+                    claw.close();
+                    stateStarted = true;
+                }
+                if (!arm.isArmBusy()) {
+                    state = State.FINISHED;
+                    stateStarted = false;
+                }
+                break;
+
+            case FINISHED:
+                telemetry.addLine("Auto State machine idle");
+                break;
+        }
+        //telemetry.addData("Limit Switch", arm.isTouchSensorPressed());
+        //telemetry.addData("Arm Position", arm.armPosition());
+        telemetry.addData("Left Motor", drivetrain.leftMotorPosition());
+        telemetry.addData("Right Motor", drivetrain.rightMotorPosition());
+        telemetry.addData("Distance Left (cm)", "%.2f", drivetrain.distanceLeftMotor());
+        telemetry.addData("Distance Right (cm)", "%.2f", drivetrain.distanceRightMotor());
+        telemetry.addData("Heading", drivetrain.classBotHeading(AngleUnit.DEGREES));
+        telemetry.addData("Turning", drivetrain.isTurnBusy());
+        telemetry.addData("Heading", drivetrain.classBotHeading(AngleUnit.DEGREES));
+        telemetry.addData("Target", drivetrain.getTargetHeading());
+        telemetry.addData("Turning", drivetrain.getTurning());
+        telemetry.addData("Left power", drivetrain.getLeftPower());
+        telemetry.addData("Right power", drivetrain.getRightPower());
+        telemetry.addData("Left mode", drivetrain.getLeftMode());
+        telemetry.addData("Right mode", drivetrain.getRightMode());
+
+    }
+}
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ClassBotDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ClassBotDrive.java
@@ -1,17 +1,29 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import static java.lang.Math.PI;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class ClassBotDrive {    //Classe avec méthodes pour le Drivetrain
 
-    private DcMotor frontLeftMotor, frontRightMotor;
-    private int leftPos,rightPos;
+    private DcMotor frontLeftMotor, frontRightMotor; // Création des moteurs
+    private static final double ticksPerRev = 636.0;   // encoder pour une rotation
+    private static final double wheelDiameter = 9.0; //diamètre de la roue du ClassBot
+    private IMU imu;  //Création du gyroscope
+    private double targetHeading;   //cible pour le virage
+    private double turnPower; // vitesse pendant le virage
+    private static final double turnTolerance = 2.0;// tolerance en degrés pour terminer le virage
+    private boolean turning = false;
 
     public void init(HardwareMap hwMap) {  //Déclaration du matériel (Utilisation de front au cas où on ajouterait 2 moteurs (back))
-        frontLeftMotor = hwMap.get(DcMotor.class, "leftmotor");
-        frontRightMotor = hwMap.get(DcMotor.class, "rightmotor");
+        frontLeftMotor = hwMap.get(DcMotor.class, "leftmotor"); //Nom identique à la configuration du driverhub
+        frontRightMotor = hwMap.get(DcMotor.class, "rightmotor"); //Nom identique à la configuration du driverhub
 
         frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);  // Dans le mode using encoder le robot essai constamment de
         frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER); // réajuster son power
@@ -21,51 +33,166 @@ public class ClassBotDrive {    //Classe avec méthodes pour le Drivetrain
 
         frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); //mode brake pour des mouvements plus précis
         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        imu = hwMap.get(IMU.class, "imu"); //Nom identique à la configuration du driverhub
+        RevHubOrientationOnRobot RevOrientation = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD,   //Le logo du Control Hub est vers l'arrière dans le ClassBot
+                RevHubOrientationOnRobot.UsbFacingDirection.UP);
+
+        imu.initialize(new IMU.Parameters(RevOrientation)); //Le port USB-C du Control Hub est vers le haut dans le ClassBot
+        imu.resetYaw();
     }
 
-    public void drive (double forward, double turn) {   //Méthode qui permet de contrôler une base tank
+    //Méthodes pour le téléop
+    public void drive(double forward, double turn) {   //Méthode qui permet de contrôler une base tank en téléop
         double leftPower = forward + turn;  // formules pour avancer et tourner
         double rightPower = forward - turn;
         double largest = Math.max(Math.abs(leftPower), Math.abs(rightPower));  // ajustement pour ne pas dépasser 1
         if (largest > 1.0) {
             leftPower /= largest;
-            rightPower/= largest;
+            rightPower /= largest;
         }
         frontLeftMotor.setPower(leftPower);
         frontRightMotor.setPower(rightPower);
     }
 
-    public void resetDriveEncoders() {    //méthode pour remettre les encodeurs à zéro au début du OpMode
+    //méthode pour remettre les encodeurs à zéro au début du OpMode
+    public void resetDriveEncoders() {
         frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
-    public int leftMotorPosition() {  //methode pour avoir le télémétrie du moteur gauche
-        return  frontLeftMotor.getCurrentPosition();
-    }
-    public int rightMotorPosition() {  //methode pour avoir le télémétrie du moteur droit
-        return  frontRightMotor.getCurrentPosition();
+
+    //méthode pour transformer les cm en ticks
+    public int cmToTicks(double distanceCm) {
+        double wheelCircumference = PI * wheelDiameter;
+        double rotations = distanceCm / wheelCircumference;
+
+        return (int) Math.round(rotations * ticksPerRev);
     }
 
-    public void driveAuto(int leftTarget, int rightTarget, double speedAuto) {
+    // Méthodes de télémétrie (encodeur, distance en cm, heading en degrés, power et modes du moteur)
+    public int leftMotorPosition() {  //methode pour avoir la télémétrie du moteur gauche
+        return frontLeftMotor.getCurrentPosition();
+    }
+    public int rightMotorPosition() {  //methode pour avoir la télémétrie du moteur droit
+        return frontRightMotor.getCurrentPosition();
+    }
+    public double distanceLeftMotor() {
+        return  (frontLeftMotor.getCurrentPosition()/ticksPerRev) * PI * wheelDiameter; // calcul de la distance en cm, roue gauche
+    }
+    public double distanceRightMotor() {
+        return  (frontRightMotor.getCurrentPosition()/ticksPerRev) * PI * wheelDiameter; // calcul de la distance en cm, roue droite
+    }
+    public double classBotHeading(AngleUnit angleUnit) {   //méthode pour avoir le heading du robot de -180 à 180 ou -pie à pie
+        return imu.getRobotYawPitchRollAngles().getYaw(angleUnit);
+    }
+    public double getTargetHeading() {
+        return targetHeading;
+    }
+    public boolean getTurning() {
+        return turning;
+    }
+    public double getLeftPower() {
+        return frontLeftMotor.getPower();
+    }
+    public double getRightPower() {
+        return frontRightMotor.getPower();
+    }
+    public DcMotor.RunMode getLeftMode() {
+        return frontLeftMotor.getMode();
+    }
+    public DcMotor.RunMode getRightMode() {
+        return frontRightMotor.getMode();
+    }
 
-        int targetLeft = frontLeftMotor.getCurrentPosition() + leftTarget;
+    //Méthodes pour le mode autonome
+    public void driveAuto(int leftTarget, int rightTarget, double speedAuto) {  // Méthode pour bouger en Autonome selon une valeur d'encodeur
+
+        int targetLeft = frontLeftMotor.getCurrentPosition() + leftTarget;  //La cible est la position actuelle + le mouvement voulu
         int targetRight = frontRightMotor.getCurrentPosition() + rightTarget;
 
-        frontLeftMotor.setTargetPosition(targetLeft);
+        frontLeftMotor.setTargetPosition(targetLeft);   //on met les moteurs à la cible
         frontRightMotor.setTargetPosition(targetRight);
 
-        frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION); // Moteur en run to position
         frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        frontLeftMotor.setPower(speedAuto);
+        frontLeftMotor.setPower(speedAuto); // le power des moteurs à speed
         frontRightMotor.setPower(speedAuto);
     }
-    public boolean isDriveBusy() {
+    public boolean isDriveBusy() {       // Vérifier si les moteurs sont occupés, que la manœuvre n'est pas terminées
         return frontLeftMotor.isBusy() || frontRightMotor.isBusy();
     }
 
+    public void driveDistance(double distanceCm, double speed) {
+        int ticks = cmToTicks(distanceCm);
+        driveAuto(ticks, ticks, speed);
     }
+
+    public void turnDegrees(String direction, double degrees, double power) { // Méthode pour tourner avec le IMU en autonome
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER); //On remet les moteurs dans le bon mode
+
+        double currentHeading = classBotHeading(AngleUnit.DEGREES);// On va lire l'angle actuel
+
+        turnPower = Math.abs(power);
+
+        if (direction.equals("RIGHT")) {
+            targetHeading = currentHeading - degrees;// si on veut virer à droite, on fait le heading - l'angle désiré
+        }
+        else if (direction.equals("LEFT")) {
+            targetHeading = currentHeading + degrees;// si on veut virer à gauche, on fait le heading + l'angle désiré
+        }
+        targetHeading = normalizeAngle(targetHeading);// correction pour gérer le passage de -180 à 180 degrés
+        turning = true;// permet de sortir of the state
+    }
+
+    private double normalizeAngle(double angle) {  // méthode pour éviter le problème du -180 qui se téléporte à 180 degrés
+        while (angle > 180) {
+            angle -= 360;
+        }
+        while (angle < -180) {
+            angle += 360;
+        }
+        return angle;
+    }
+
+    // méthode pour vérifier si la manœuvre est terminée et donner le bon power aux moteurs
+
+    public boolean isTurnBusy() {
+        if (!turning) {
+            return false;
+        }
+        double currentHeading =
+                classBotHeading(AngleUnit.DEGREES);
+        double error =
+                normalizeAngle(targetHeading - currentHeading);
+        // Vérification si on est à la cible
+        if (Math.abs(error) <= turnTolerance) {
+            frontLeftMotor.setPower(0);
+            frontRightMotor.setPower(0);
+            turning = false;
+            return false;
+        }
+        // sinon, on continue à tourner dans le bon sens
+        if (error > 0) {
+            // Tourner vers la gauche
+            frontLeftMotor.setPower(-turnPower); // moteur gauche recule, moteur droit avance
+            frontRightMotor.setPower(turnPower);
+        }
+        else {
+            // Tourner vers la droite
+            frontLeftMotor.setPower(turnPower);// moteur gauche avance, moteur droit recule
+            frontRightMotor.setPower(-turnPower);
+        }
+        return true;
+    }
+
+
+
+
+}
 

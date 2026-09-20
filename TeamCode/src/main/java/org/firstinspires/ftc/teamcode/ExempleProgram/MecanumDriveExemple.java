65b56bd80a88f568cabbe27391202cfe3ad61e60
AdminCuivreOr <168549335+seblamp@users.noreply.github.com>
2026-09-20 09:22:58 -0400

    Code exemple pour base mecanum
    Field et robot oriented avec toggle entre les deux

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExempleProgram/MecanumDriveExemple.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode.ExempleProgram;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+
+public class MecanumDriveExemple {
+    private DcMotor frontLeft, backLeft, frontRight, backRight; // Déclaration des moteurs
+    private IMU imu;    //Déclaration du Gyro du ControlHub
+
+    //Méthode qui initialise les moteurs et le IMU
+    public void init (HardwareMap hwMap) {
+        frontLeft = hwMap.get(DcMotor.class, "frontLeft"); //Les noms en vert doivent être identiques à la configuration sur le DriverHub
+        backLeft = hwMap.get(DcMotor.class, "backLeft");
+        frontRight = hwMap.get(DcMotor.class, "frontRight");
+        backRight = hwMap.get(DcMotor.class, "backRight");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);  //Obverver la base lorsqu'elle avance et inverser les moteurs au besoin
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);   //Les moteurs utilisent sont PID interne pour toujours avoir le bon power
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);    //Les moteurs sont en mode brake quand il n'y a pas de signal de la manette
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        imu = hwMap.get(IMU.class,"imu"); // imu devrait être le mom par défault sur la configuration du DriverHub
+        RevHubOrientationOnRobot RevOrientation = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,     //Changer UP et FOWARD pour la réalité du robot
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD); //Le ControlHub devrait toujours être dans un angle droit par rapport à la structure du robot
+
+        imu.initialize(new IMU.Parameters(RevOrientation));
+    }
+
+    // Méthode pour la base en RobotOriented,
+    public void drive  (double forward, double strafe, double rotate){   //déclaration des variables
+        double frontLeftPower = forward + strafe + rotate;  //Formules qui gèrent le power aux roues.
+        double backLeftPower = forward + strafe + rotate;      // 1. Ajuster avance/recule en inversant les moteurs au besoin
+        double frontRightPower = forward + strafe + rotate;  //2. Tester le strafe et modifier le signe +/- devant strafe dans les formules au besoin
+        double backRightPower = forward + strafe + rotate;   //3. Tester le rotate et modifier le signe +/- devant rotate dans les formules au besoin
+
+        double maxPower = 1.0;
+        double maxSpeed = 1.0;  //Ajustement de la vitesse maximale au besoin
+
+        maxPower = Math.max(maxPower, Math.abs(frontLeftPower)); // On trouve le plus grand power demandé par les 4 moteurs.
+        maxPower = Math.max(maxPower, Math.abs(backLeftPower));  // Le minimum est 1.0 et on divise uniquement si on a un input plus grand que 1.0
+        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backRightPower));
+
+        frontLeft.setPower(maxSpeed*(frontLeftPower/maxPower));  //Calcul du power de chaque moteur qui ne peut pas dépasser 1.0
+        backLeft.setPower(maxSpeed*(backLeftPower/maxPower));
+        frontRight.setPower(maxSpeed*(frontRightPower/maxPower));
+        backRight.setPower(maxSpeed*(backRightPower/maxPower));
+    }
+
+    //Méthode pour piloter en Field Oriented
+    public void  driveFieldRelative(double forward, double strafe, double rotate) {
+        double theta = Math.atan2(forward, strafe);  //On transforme les valeurs en coordonnées polaires avec de la trigonométrie
+        double r = Math.hypot(strafe,forward);
+
+        theta = AngleUnit.normalizeRadians(theta - imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+
+        double newForward = r * Math.sin(theta);  // retransformation en coordonnées cartésiennes
+        double newStrafe = r * Math.cos(theta);
+
+        this.drive(newForward, newStrafe, rotate);  // On appelle la méthode drive avec les nouvelles variables
+
+    }
+    public void resetYaw() {
+        imu.resetYaw();}  //Méthode pour remettre l'orientation du robot à zéro
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExempleProgram/MecanumOpModeExemple.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.ExempleProgram;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp  //TeleOp, Autonomous ou Disabled
+
+public class MecanumOpModeExemple extends OpMode {
+
+    MecanumDriveExemple drive = new MecanumDriveExemple();  //Création de l'instance Mecanum drive à partir de notre classe MecanumDriveExemple
+
+    double forward, strafe, rotate;  //Variables pour les contrôles
+    boolean fieldOriented = true;       //Variables pour passer de Robot à Field Oriented
+    boolean previousBackButton = false;
+    boolean previousStartButton = false;
+
+
+    @Override
+    public void init() {
+        drive.init(hardwareMap);  //Initialisation du matériel
+    }
+
+    @Override
+    public void loop() {
+        //forward = gamepad1.left_stick_y;  //Attribution des contrôles
+        forward = (gamepad1.right_trigger - gamepad1.left_trigger); // enlever le commentaire et mettre devant l'autre forward pour contrôler avec les triggers ou le joystick
+        strafe = gamepad1.left_stick_x;
+        rotate = gamepad1.right_stick_x;
+
+        if (gamepad1.back && !previousBackButton) {   // Passage de Robot à Field Oriented
+            fieldOriented = !fieldOriented;
+        }
+
+        previousBackButton = gamepad1.back;
+
+        if (fieldOriented) {
+            drive.driveFieldRelative(forward, strafe, rotate);  // Méthode à utiliser si Field Oriented
+        } else {
+            drive.drive(forward, strafe, rotate);       //Méthode à utiliser si robot Oriented
+        }
+
+        if (gamepad1.start && !previousStartButton) {   // Bouton pour remettre l'angle du gyro à 0
+            drive.resetYaw();
+        }
+
+        previousStartButton = gamepad1.start;
+
+        telemetry.addData(       //Télémétrie pour savoir si on est en field ou robot oriented
+                "Mode",
+                fieldOriented ? "FIELD ORIENTED" : "ROBOT ORIENTED"
+        );
+        telemetry.update();
+    }
+
+}
+
